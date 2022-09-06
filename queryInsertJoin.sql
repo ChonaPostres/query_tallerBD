d571e403435bded72319aad061625d09efd2bc5d
Ignacio Araya <vicariousmen@gmail.com>
2022-09-06 00:20:35 -0300

    insert join procedencias

--- queryInsertJoin.sql
+++ queryInsertJoin.sql
@@ -4,6 +4,13 @@ insert into organismospublicos(codigoorganismopublico, organismopublico, sector)
 select distinct traspaso.codigoorganismopublico::integer, traspaso.organismopublico, sectores.idsector
 from traspaso
 join sectores
-on (traspaso.sector = sectores.sector)
-
+on (traspaso.sector = sectores.sector);
 --delete from organismospublicos;
+
+--Procedencias--
+insert into procedencias(procedencia, tipo)
+select distinct traspaso.procedencia, tipos.tipo
+from traspaso
+join tipos
+on (traspaso.tipo = tipos.tipo);
+--delete from procedencias;
