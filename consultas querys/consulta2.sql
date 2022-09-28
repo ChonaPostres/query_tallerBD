a65a4c39cc680ddcc84780a5db2df4f4f26c45c3
Ignacio Araya <vicariousmen@gmail.com>
2022-09-27 21:42:13 -0300

    creacion base de datos taller2022 y consultas parte 1 y 2 ok

--- consultas querys/consulta2.sql
+++ consultas querys/consulta2.sql
@@ -1,8 +1,16 @@
 ------Parte 1------
-select unidadescompras.unidadcompra, sum(totallineaneto) from itemeslicitaciones
+select unidadescompras.unidadcompra, sum(round(totallineaneto)) from itemeslicitaciones
 join licitaciones ON licitaciones.idlicitacion = itemeslicitaciones.idlicitacion
 join unidadescompras ON unidadescompras.codigounidadcompra = licitaciones.codigounidadcompra
 group by unidadescompras.unidadcompra
-having sum(totallineaneto) > 1000000000;
+having sum(round(totallineaneto)) > 1000000000;
 ------Parte 2------
+select unidadescompras.unidadcompra, rubros3.rubro, sum(round(totallineaneto)) from itemeslicitaciones
+join licitaciones ON licitaciones.idlicitacion = itemeslicitaciones.idlicitacion
+join unidadescompras ON unidadescompras.codigounidadcompra = licitaciones.codigounidadcompra
+join productos ON productos.codigoproducto = itemeslicitaciones.codigoproducto
+join rubros ON rubros.id_rubro = productos.rubro
+join rubros rubros2 ON rubros2.id_rubro = rubros.rubropadre
+join rubros rubros3 ON rubros3.id_rubro = rubros2.rubropadre
+group by unidadescompras.unidadcompra, rubros3.rubro;
 ------Parte 3------
---
+++ creacionDB.sql
@@ -0,0 +1,8 @@
+create database taller2022
+with
+owner = postgres
+encoding = 'UTF8'
+LC_COLLATE = 'Spanish_Chile.1252'
+LC_CTYPE = 'Spanish_Chile.1252'
+tablespace = pg_default
+connection limit = -1;
