c0afeeb0e39a828496556a1cca867fd09cc2ebe6
Ignacio Araya <vicariousmen@gmail.com>
2022-10-11 01:31:05 -0300

    consulta3 parte 3 casi ok

--- consultas querys/consulta3.sql
+++ consultas querys/consulta3.sql
@@ -12,3 +12,21 @@ having sum(IL.totallineaneto) >  all (
 	where UC2.unidadcompra <> UC.unidadcompra
 	group by UC2.unidadcompra
 )
+
+------Parte 2------
+------Parte 3------
+select P1.nombreproducto, count(*)
+from itemeslicitaciones as IL
+join licitaciones ON licitaciones.idlicitacion = IL.idlicitacion
+join productos as P1 ON P1.codigoproductos = IL.codigoproducto
+group by P1.nombreproducto
+having count(*) > any (
+	select avg(count_productos)
+	from (
+		select count(*) as count_productos
+		from itemeslicitaciones as IL2
+		join licitaciones ON licitaciones.idlicitacion = IL2.idlicitacion
+		join productos as P2 ON P2.codigoproductos = IL2.codigoproducto
+		group by P2.nombreproducto
+	) as Z
+)
