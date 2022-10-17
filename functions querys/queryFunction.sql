8aa988d83e7ca4abdc90cec6dc43747770845cf9
Nilemo <nicolas.leiva.moya@gmail.com>
2022-10-17 00:19:25 -0300

    solucion pregunta 3

--- functions querys/queryFunction.sql
+++ functions querys/queryFunction.sql
@@ -10,6 +10,8 @@ begin
 		return 1;
 	elseif (validarcaracteresrut(rut) = false) then 
 		return 2;
+	elseif (validarformatorut(rut) = false) then 
+		return 3;
 	end if;
 	return 0;
 end;
@@ -32,7 +34,8 @@ begin
 	   substr(rut, count, 1) != '6' and substr(rut, count, 1) != '7' and
 	   substr(rut, count, 1) != '8' and substr(rut, count, 1) != '9' and
 	   substr(rut, count, 1) != '.' and substr(rut, count, 1) != '-' and
-	   substr(rut, count, 1) != 'k') then
+	   substr(rut, count, 1) != 'k' and substr(rut, count, 1) != '-' and
+	   substr(rut, count, 1) != 'K') then
 		--raise notice 'false';
 		return false;
 	end if;
@@ -45,7 +48,28 @@ begin
 end;
 $$
 language plpgsql;
-
+create or replace function ValidarFormatoRUT (rut character varying (12)) returns boolean
+as
+$$
+declare
+	--count integer;
+begin
+	if length(rut)>12 then --quita los posibles mayores de 12 caracteres
+		return false;
+	elseif
+		left(right(rut,(2)), 1) != '-' then--verifica que el - este antes del codigo verificador
+	return false;
+		elseif
+			rut not like '%.%.%' and length(rut)> 10 then -- verifica que si tiene mas de 10 caracts que use el formato con puntos
+		return false;
+			elseif
+				rut like '%.%.%' and length(rut)<= 10 then -- verifica que si tiene menos de 10 caracts que no use el formato con puntos
+			return false;
+	else return true;
+	end if;
+end;
+$$
+language plpgsql;
 -- Consulta a tabla unidadescompras para ver los rutunidadcompra que sean invalidos 
 select rutunidadcompra, validarut(rutunidadcompra) from unidadescompras
 where validarut(rutunidadcompra) <> 0;
