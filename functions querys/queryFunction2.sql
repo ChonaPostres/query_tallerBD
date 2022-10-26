b4ae8b894da5efc974670bccf5e475cf3748512f
Ignacio Araya <vicariousmen@gmail.com>
2022-10-26 00:13:56 -0300

    queryFunction2 parte 1 ok

--- functions querys/queryFunction2.sql
+++ functions querys/queryFunction2.sql
@@ -1,3 +1,4 @@
+-- Parte 1
 create or replace function Listaritemescompradosendedor (
     cod_organismopublico integer, 
 	cod_unidadcompra integer,
@@ -43,17 +44,20 @@ begin
 	join rubros as rubron2 ON rubron2.id_rubro = rubron1.rubropadre
 	join rubros as rubron3 ON rubron3.id_rubro = rubron2.rubropadre
 	where 
-		cod_organismopublico = organismospublicos.codigoorganismopublico or cod_organismopublico is null and
-		cod_unidadcompra = unidadescompras.codigounidadcompra or cod_unidadcompra is null and
-		cod_sector = sectores.idsector or cod_sector is null and
-		nombre_proveedor = proveedores.nombreproveedor or nombreproveedor is null and
-		nombre_producto = productos.nombreproducto or nombreproducto is null and
-		cod_rubron1 = rubron1.id_rubro or cod_rubron1 is null and 
-		cod_rubron3 = rubron3.id_rubro or cod_rubron3 is null and
-		fechainicio <= licitaciones.fechaaceptacion or fechainicio is null and
-		licitaciones.fechaaceptacion <= fechafinal or fechafinal is null;
+		(cod_organismopublico = organismospublicos.codigoorganismopublico or cod_organismopublico is null) and
+		(cod_unidadcompra = unidadescompras.codigounidadcompra or cod_unidadcompra is null) and
+		(cod_sector = sectores.idsector or cod_sector is null) and
+		(nombre_proveedor = proveedores.nombreproveedor or nombre_proveedor is null) and
+		(nombre_producto = productos.nombreproducto or nombre_producto is null) and
+		(cod_rubron1 = rubron1.id_rubro or cod_rubron1 is null) and 
+		(cod_rubron3 = rubron3.id_rubro or cod_rubron3 is null) and
+		(licitaciones.fechaaceptacion between fechainicio and fechafinal 
+		or fechainicio is null or fechafinal is null)
+		;
 end;
 $$
 language plpgsql;
-
-select * from Listaritemescompradosendedor(null, null, null, null, null, null, null, date '2019/03/01', date '2019/04/01')
+-- Ejemplos de uso
+select * from Listaritemescompradosendedor(7231, 1881, null, 'JUAN CARLOS', null, null, null, date '2019/03/01', date '2019/04/01');
+select * from Listaritemescompradosendedor(7231, null, null, null, null, null, null, date '2019/03/01', date '2019/04/01');
+select * from Listaritemescompradosendedor(null, null, null, null, null, null, null, null, null);
