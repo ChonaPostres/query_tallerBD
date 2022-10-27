bfeb9d6ddfcf4700565ee92e601600d09bdd5ad2
Ignacio Araya <vicariousmen@gmail.com>
2022-10-26 21:45:18 -0300

    queryFunction2 Parte 2 N3 ok, falta N3 x Productos

--- functions querys/queryFunction2.sql
+++ functions querys/queryFunction2.sql
@@ -62,39 +62,43 @@ select * from Listaritemescompradosendedor(7231, 1881, null, 'JUAN CARLOS', null
 select * from Listaritemescompradosendedor(7231, null, null, null, null, null, null, date '2019/03/01', date '2019/04/01');
 select * from Listaritemescompradosendedor(null, null, null, null, null, null, null, null, null);
 
-create type t_prodvalor as (idrubro integer, rubro varchar(255));
+create type t_prodvalor as (idrubro integer, rubro varchar(255), montogastado bigint);
 create or replace function listamontosxrubros (fechaaceptacion_inicio date, Fechaaceptacion_final date) returns setof t_prodvalor
 as 
 $$
+
 declare
-	nivel1 cursor for select id_rubro, rubro from rubros where rubropadre is null;
-	nivel2 cursor (padre integer) for select id_rubro, rubro from rubros where rubropadre = padre;
+	nivel1 cursor for select id_rubro, rubro, 0::bigint from rubros where rubropadre is null;
+	nivel2 cursor (padre integer) for select rubros.id_rubro, rubros.rubro, 0::bigint from rubros where rubropadre = padre;
+	nivel3 cursor (padre integer) for select rubros.id_rubro, rubros.rubro, sum(itemeslicitaciones.totallineaneto) from rubros join productos ON productos.rubro = rubros.id_rubro join itemeslicitaciones ON itemeslicitaciones.codigoproducto = productos.codigoproductos where rubropadre = padre group by rubros.id_rubro, rubros.rubro;
 	registro1 t_prodvalor;
 	registro2 t_prodvalor;
-	agrupacionmontogastado1 bigint;
-	agrupacionmontogastado2 bigint;
+	registro3 t_prodvalor;
 begin
-	agrupacionmontogastado1 = 0;
-	agrupacionmontogastado2 = 0;
 	for registro1 in nivel1 loop
 		return next registro1;
-		agrupacionmontogastado1 = agrupacionmontogastado1 + (select totallineaneto from rubros join productos ON productos.rubro = registro1.id_rubro join itemeslicitaciones ON itemeslicitaciones.codigoproducto = productos.codigoproductos where rubros.id_rubro = registro1.id_rubro);
-		raise notice 'agrupacionmontogastado1: %', agrupacionmontogastado1;
-		
 		open nivel2 (padre:=registro1.id_rubro);
+		raise notice 'registro1: %', registro1.id_rubro;
 		loop
 			fetch next from nivel2 into registro2;
-			agrupacionmontogastado2 = agrupacionmontogastado2 + (select totallineaneto from rubros join productos ON productos.rubro = registro1.id_rubro join itemeslicitaciones ON itemeslicitaciones.codigoproducto = productos.codigoproductos where rubros.id_rubro = registro2.idrubro);
-			raise notice 'agrupacionmontogastado2: %', agrupacionmontogastado2;
 			exit when not found;
 			return next registro2;
+			raise notice 'registro2: %', registro2.idrubro;
+			open nivel3 (padre:=registro2.idrubro);
+			loop
+				fetch next from nivel3 into registro3;
+				exit when not found;
+				return next registro3;
+				raise notice 'registro3: %', registro3.idrubro;
+			end loop;
+			close nivel3;
 		end loop;
 		close nivel2;
 	end loop;
-	raise notice 'agrupacionmontogastado1: %', agrupacionmontogastado1;
-	raise notice 'agrupacionmontogastado2: %', agrupacionmontogastado2;
 end;
 $$
 language plpgsql;
 
 select * from listamontosxrubros(date '2019/03/01', date '2019/04/01');
+
+select rubros.id_rubro, rubros.rubro, sum(itemeslicitaciones.totallineaneto) from rubros join rubros rubros2 ON rubros2.id_rubro = rubros.rubropadre join rubros rubros3 ON rubros3.id_rubro = rubros2.rubropadre join productos ON productos.rubro = rubros.id_rubro join itemeslicitaciones ON itemeslicitaciones.codigoproducto = productos.codigoproductos group by rubros.id_rubro, rubros.rubro;
