f82c1ef00bb79b63a53553611602ed892760f2df
Ignacio Araya <vicariousmen@gmail.com>
2022-12-01 20:15:57 -0300

    proyecto ofertas de autos: creación y inserción de tablas

---
+++ ProyectoCarSales/creacionTablas.sql
@@ -0,0 +1,167 @@
+create table empresas(
+id_empresa serial not null,
+empresa varchar(100) not null,
+constraint pk_empresas primary key (id_empresa)
+);
+
+create table condicion_cuerpos(
+id_condicion_cuerpo serial not null,
+condicion_cuerpo varchar(100) not null,
+constraint pk_condicion_cuerpos primary key (id_condicion_cuerpo)
+);
+
+create table condicion_mecanicas(
+id_condicion_mecanica serial not null,
+condicion_mecanica varchar(100) not null,
+constraint pk_condicion_mecanicas primary key (id_condicion_mecanica)
+);
+
+create table tipo_vendedores(
+id_tipo_vendedor serial not null,
+tipo_vendedor varchar(100) not null,
+constraint pk_tipo_vendedores primary key (id_tipo_vendedor)
+);
+
+create table tipo_cuerpos(
+id_tipo_cuerpo serial not null,
+tipo_cuerpo varchar(100) not null,
+constraint pk_tipo_cuerpos primary key (id_tipo_cuerpo)
+);
+
+create table colores(
+id_color serial not null,
+color varchar(100) not null,
+constraint pk_colores primary key (id_color)
+);
+
+create table cilindros(
+id_cilindro serial not null,
+cilindro varchar(100) not null,
+constraint pk_cilindros primary key (id_cilindro)
+);
+
+create table specs_regionales(
+id_specs_regional serial not null,
+specs_regional varchar(100) not null,
+constraint pk_specs_regionales primary key (id_specs_regional)
+);
+
+create table horse_powers(
+id_horse_power serial not null,
+horse_power varchar(100) not null,
+constraint pk_horse_powers primary key (id_horse_power)
+);
+
+create table costados_manubrio(
+id_costado_manubrio serial not null,
+costado_manubrio varchar(100) not null,
+constraint pk_costados_manubrio primary key (id_costado_manubrio)
+);
+
+create table tipo_transmisiones(
+id_tipo_transmision serial not null,
+tipo_transmision varchar(50) not null,
+constraint pk_tipo_transmisiones primary key (id_tipo_transmision)
+);
+
+create table tipo_combustibles(
+id_tipo_combustible serial not null,
+tipo_combustible varchar(50) not null,
+constraint pk_tipo_combustibles primary key (id_tipo_combustible)
+);
+
+create table motores(
+id_motor serial not null,
+motor varchar(50) not null,
+combustible int not null,
+horse_power int not null,
+cilindros int not null,
+constraint pk_motores primary key (id_motor),
+constraint fk_motores_tipo_combustibles
+   foreign key (combustible) references tipo_combustibles(id_tipo_combustible),
+constraint fk_motores_horse_powers
+   foreign key (horse_power) references horse_powers(id_horse_power), 
+constraint fk_motores_cilindros
+   foreign key (cilindros) references cilindros(id_cilindro) 
+);
+
+create table estados(
+id_estado serial not null,
+kilometros int not null,
+condicion_cuerpo int not null,
+condicion_mecanica int not null,
+constraint pk_estados primary key (id_estado),
+constraint fk_estados_condicion_cuerpos
+   foreign key (condicion_cuerpo) references condicion_cuerpos(id_condicion_cuerpo),
+constraint fk_estados_condicion_mecanicas
+   foreign key (condicion_mecanica) references condicion_mecanicas(id_condicion_mecanica)
+);
+
+create table caracteristicas(
+id_caracteristica serial not null,
+color int not null,
+specs_regional int not null,
+costado_manubrio int not null,
+tipo_transmision int not null,
+constraint pk_caracteristicas primary key (id_caracteristica),
+constraint fk_caracteristicas_specs_regionales
+   foreign key (specs_regional) references specs_regionales(id_specs_regional),
+constraint fk_caracteristicas_costados_manubrio
+   foreign key (costado_manubrio) references costados_manubrio(id_costado_manubrio),
+constraint fk_caracteristicas_colores
+   foreign key (color) references colores(id_color),
+constraint fk_caracteristicas_tipo_transmisiones
+   foreign key (tipo_transmision) references tipo_transmisiones(id_tipo_transmision)
+);
+
+create table anios(
+id_anio serial not null,
+anio varchar(10) not null,
+constraint pk_anio primary key (id_anio)
+);
+
+create table modelos(
+id_modelo serial not null,
+modelo varchar(100) not null,
+empresa int not null,
+constraint pk_modelos primary key (id_modelo),
+constraint fk_modelos_empresas
+   foreign key (empresa) references empresas(id_empresa) 
+);
+
+create table vehiculos(
+id_vehiculo serial not null,
+modelo int not null,
+tipo_cuerpo int not null,
+anio int not null,
+caracteristica integer not null,
+estado integer not null,
+motor integer not null,
+constraint pk_vehiculos primary key (id_vehiculo),
+constraint fk_vehiculos_modelos
+   foreign key (modelo) references modelos(id_modelo),
+constraint fk_vehiculos_tipo_cuerpos
+   foreign key (tipo_cuerpo) references tipo_cuerpos(id_tipo_cuerpo),
+constraint fk_vehiculos_anios
+   foreign key (anio) references anios(id_anio),
+constraint fk_vehiculos_caracteristicas
+   foreign key (caracteristica) references caracteristicas(id_caracteristica),
+constraint fk_vehiculos_estados
+   foreign key (estado) references estados(id_estado),
+constraint fk_vehiculos_motores
+   foreign key (motor) references motores(id_motor)
+);
+
+create table ofertas(
+id_oferta serial not null,
+oferta varchar(255) not null,
+price_aed int not null,
+vehiculo int not null,
+tipo_vendedor int not null,
+date_posted date default to_date('1/1/1900','DD/MM/YYYY'),
+constraint pk_ofertas primary key (id_oferta),
+constraint fk_ofertas_vehiculos
+   foreign key (vehiculo) references vehiculos(id_vehiculo),
+constraint fk_ofertas_tipo_vendedores
+   foreign key (tipo_vendedor) references tipo_vendedores(id_tipo_vendedor) 
+);
---
+++ ProyectoCarSales/insertTablas.sql
@@ -0,0 +1,153 @@
+-- Tabla empresas
+insert into empresas(empresa)
+select distinct company
+from traspaso order by company;
+
+-- Tabla condicion_cuerpos
+insert into condicion_cuerpos(condicion_cuerpo)
+select distinct body_condition
+from traspaso order by body_condition;
+
+-- Tabla condicion_mecanicas
+insert into condicion_mecanicas(condicion_mecanica)
+select distinct body_condition
+from traspaso order by body_condition;
+
+-- Tabla tipo_vendedores
+insert into tipo_vendedores(tipo_vendedor)
+select distinct seller_type
+from traspaso order by seller_type;
+
+-- Tabla tipo_cuerpos
+insert into tipo_cuerpos(tipo_cuerpo)
+select distinct body_type
+from traspaso order by body_type;
+
+-- Tabla colores
+insert into colores(color)
+select distinct color
+from traspaso order by color;
+
+-- Tabla cilindros
+insert into cilindros(cilindro)
+select distinct case when no_of_cylinders is null then 'Unknown' else no_of_cylinders end
+from traspaso order by no_of_cylinders;
+
+-- Tabla specs_regionales
+insert into specs_regionales(specs_regional)
+select distinct regional_specs
+from traspaso order by regional_specs;
+
+-- Tabla horse_powers
+insert into horse_powers(horse_power)
+select distinct horsepower
+from traspaso order by horsepower;
+
+-- Tabla costados_manubrio
+insert into costados_manubrio(costado_manubrio)
+select distinct steering_side
+from traspaso order by steering_side;
+
+-- Tabla anios
+insert into anios(anio)
+select distinct case when "year" is null then 'Unknown' else "year" end 
+from traspaso order by "year";
+
+-- Tabla costados_manubrio
+insert into costados_manubrio(costado_manubrio)
+select distinct steering_side
+from traspaso order by steering_side;
+
+-- Tabla tipo_transmisiones
+insert into tipo_transmisiones(tipo_transmision)
+select distinct transmission_type
+from traspaso order by transmission_type;
+
+-- Tabla tipo_combustibles
+insert into tipo_combustibles(tipo_combustible)
+select distinct fuel_type 
+from traspaso order by fuel_type;
+
+-- Tabla motores
+insert into motores(motor, combustible, horse_power, cilindros)
+select distinct case when motors_trim is null then 'Other' else motors_trim end, tipo_combustibles.id_tipo_combustible, horse_powers.id_horse_power, cilindros.id_cilindro
+from traspaso 
+join tipo_combustibles on (tipo_combustibles.tipo_combustible = fuel_type)
+join horse_powers on (horse_powers.horse_power = horsepower)
+join cilindros on (cilindros.cilindro = no_of_cylinders)
+order by motors_trim;
+--delete from motores;
+
+-- Tabla modelos
+insert into modelos(modelo, empresa)
+select distinct model, empresas.id_empresa
+from traspaso 
+join empresas on empresas.empresa = traspaso.company
+order by model;
+
+-- Tabla estados
+insert into estados(kilometros, condicion_cuerpo, condicion_mecanica)
+select distinct kilometers::integer, condicion_cuerpos.id_condicion_cuerpo, condicion_mecanicas.id_condicion_mecanica
+from traspaso 
+join condicion_cuerpos on (condicion_cuerpos.condicion_cuerpo = body_condition)
+join condicion_mecanicas on (condicion_mecanicas.condicion_mecanica = mechanical_condition)
+order by kilometers;
+
+-- Tabla caracteristicas
+insert into caracteristicas(color, specs_regional, costado_manubrio, tipo_transmision)
+select distinct colores.id_color, specs_regionales.id_specs_regional, costados_manubrio.id_costado_manubrio, tipo_transmisiones.id_tipo_transmision
+from traspaso 
+join colores on (colores.color = traspaso.color)
+join specs_regionales on (specs_regionales.specs_regional = traspaso.regional_specs)
+join costados_manubrio on (costados_manubrio.costado_manubrio = traspaso.steering_side)
+join tipo_transmisiones on (tipo_transmisiones.tipo_transmision = traspaso.transmission_type);
+
+-- Tabla vehiculos
+insert into vehiculos(modelo, tipo_cuerpo, anio, caracteristica, estado, motor)
+select modelos.id_modelo, tipo_cuerpos.id_tipo_cuerpo, anios.id_anio, caracteristicas.id_caracteristica, estados.id_estado, motores.id_motor
+from traspaso as t
+join modelos on (modelos.modelo = t.model)
+join tipo_cuerpos on (tipo_cuerpos.tipo_cuerpo = t.body_type)
+join anios on (anios.anio = t."year")
+join colores on (colores.color = t.color)
+join specs_regionales on (specs_regionales.specs_regional = t.regional_specs)
+join costados_manubrio on (costados_manubrio.costado_manubrio = t.steering_side)
+join tipo_transmisiones on (tipo_transmisiones.tipo_transmision = t.transmission_type)
+join caracteristicas on (caracteristicas.color = colores.id_color and caracteristicas.specs_regional = specs_regionales.id_specs_regional and
+caracteristicas.costado_manubrio = costados_manubrio.id_costado_manubrio and caracteristicas.tipo_transmision = tipo_transmisiones.id_tipo_transmision)
+join condicion_cuerpos on (condicion_cuerpos.condicion_cuerpo = t.body_condition)
+join condicion_mecanicas on (condicion_mecanicas.condicion_mecanica = t.mechanical_condition)
+join estados on (estados.kilometros = t.kilometers::integer and estados.condicion_cuerpo = condicion_cuerpos.id_condicion_cuerpo and 
+estados.condicion_mecanica = condicion_mecanicas.id_condicion_mecanica)
+join tipo_combustibles on (tipo_combustibles.tipo_combustible = t.fuel_type)
+join horse_powers on (horse_powers.horse_power = t.horsepower)
+join cilindros on (cilindros.cilindro = t.no_of_cylinders)
+join motores on (motores.motor = (case when t.motors_trim is null then 'Other' else t.motors_trim end) and motores.combustible = tipo_combustibles.id_tipo_combustible
+and motores.horse_power = horse_powers.id_horse_power and motores.cilindros = cilindros.id_cilindro);
+
+-- Tabla ofertas
+insert into ofertas(oferta, price_aed, vehiculo, tipo_vendedor, date_posted)
+select case when t.title is null then 'Unknown' else t.title end, (replace(price_in_aed, ',','')::integer), vehiculos.id_vehiculo, tipo_vendedores.id_tipo_vendedor, TO_DATE(t.date_posted, 'dd/mm/yyyy')
+from traspaso as t
+join tipo_vendedores on (tipo_vendedores.tipo_vendedor = t.seller_type)
+join modelos on (modelos.modelo = t.model)
+join tipo_cuerpos on (tipo_cuerpos.tipo_cuerpo = t.body_type)
+join anios on (anios.anio = t."year")
+join colores on (colores.color = t.color)
+join specs_regionales on (specs_regionales.specs_regional = t.regional_specs)
+join costados_manubrio on (costados_manubrio.costado_manubrio = t.steering_side)
+join tipo_transmisiones on (tipo_transmisiones.tipo_transmision = t.transmission_type)
+join caracteristicas on (caracteristicas.color = colores.id_color and caracteristicas.specs_regional = specs_regionales.id_specs_regional and
+caracteristicas.costado_manubrio = costados_manubrio.id_costado_manubrio and caracteristicas.tipo_transmision = tipo_transmisiones.id_tipo_transmision)
+join condicion_cuerpos on (condicion_cuerpos.condicion_cuerpo = t.body_condition)
+join condicion_mecanicas on (condicion_mecanicas.condicion_mecanica = t.mechanical_condition)
+join estados on (estados.kilometros = t.kilometers::integer and estados.condicion_cuerpo = condicion_cuerpos.id_condicion_cuerpo and 
+estados.condicion_mecanica = condicion_mecanicas.id_condicion_mecanica)
+join tipo_combustibles on (tipo_combustibles.tipo_combustible = t.fuel_type)
+join horse_powers on (horse_powers.horse_power = t.horsepower)
+join cilindros on (cilindros.cilindro = t.no_of_cylinders)
+join motores on (motores.motor = (case when t.motors_trim is null then 'Other' else t.motors_trim end) and motores.combustible = tipo_combustibles.id_tipo_combustible
+and motores.horse_power = horse_powers.id_horse_power and motores.cilindros = cilindros.id_cilindro)
+join vehiculos on (vehiculos.modelo = modelos.id_modelo and vehiculos.tipo_cuerpo = tipo_cuerpos.id_tipo_cuerpo and vehiculos.anio = anios.id_anio and
+vehiculos.caracteristica = caracteristicas.id_caracteristica and vehiculos.estado = estados.id_estado and vehiculos.motor = motores.id_motor);
+
